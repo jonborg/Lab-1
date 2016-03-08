5bd5be4a8429006b79c3ea572c7a2d8ebd890de3
jonborg <joaomanborges@hotmail.com>
2016-03-08 22:59:22 +0000

    Position output
    Position output was added

--- pro1.m
+++ pro1.m
@@ -1,5 +1,9 @@
-function [dof] = pro1(teta)
+function [pose] = pro1(teta)
 %pro1 : Computes the pose of the hand of a 6-dof arm-robot (should do it...)
+%   Receives variable teta of dimensions [1 6] and using moving axes
+%   rotation matrixes, returns vector pose of imensions [1 6] that gives
+%   (x,y,z,alpha,beta,gamma) parameters of the hand
+%
 %   teta=[0 pi/4 -pi/4 0 0 anything] should give a similar position to the
 %   figure1 in the lab assignment 
 
@@ -30,7 +34,7 @@ dof(:,5)=T01*T12*T23*T34*T45*[0 0 0 1]';
 R56=[cos(teta(6)) 0 sin(teta(6)); 0 1 0; -sin(teta(6)) 0 cos(teta(6))]; %rotação em torno de y
 P56=[0 20*10^-3 0]'; %ampliado para testes
 T56=[R56 P56;0 0 0 1];
-dof(:,6)=T01*T12*T23*T34*T45*T56*[0 0 0 1]';
+dof(:,6)=T01*T12*T23*T34*T45*T56*[0 0 0 1]'
 
 plot3(dof(1,:),dof(2,:),dof(3,:),'Marker','o');
 title('Robot Arm');
@@ -38,6 +42,9 @@ xlabel('x');
 ylabel('y');
 zlabel('z');
 axis([-0.3 0.4 -0.3 0.4 -0.3 0.4]);
+
+
+pose=[dof(21),dof(22),dof(23)]
 
 end
 
